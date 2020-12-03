da1c07866e7b6275390bdda32733520a2441b463
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-12-03 17:23:52 +0300

    fix: fix solution

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -86,9 +86,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {
-  let x = ((-1) * b) / a;
-  return x;
+function getLinearEquationRoot(/* a, b */) {
+  throw new Error('Not implemented');
 }
 
 
