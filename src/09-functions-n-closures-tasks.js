aa99403b13df900d19436b8d6a2cdc7c803ea1b9
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-12-15 01:24:38 +0300

    feat: add soltuin

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -83,8 +83,13 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  return () => {
+    if (!this.res) {
+      this.res = func();
+    }
+    return this.res;
+  };
 }
 
 
