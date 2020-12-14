8899429764843d39f5e3e4f191b3f10208913f76
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-12-15 01:14:26 +0300

    feat: add solution

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -149,8 +149,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function applyArgs(...args2) {
+    return fn.apply(null, [...args1, ...args2]);
+  };
 }
 
 
