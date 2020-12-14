4ac6a3e7df6c6a5e91a98f561185fdeedbfe4787
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-12-14 23:51:49 +0300

    fix: fix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -275,7 +275,7 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-    return arr.map((e, i) => i % 2 === 1 & e).filter((e) => e !== false);
+  return arr.map((e, i) => i % 2 === 1 & e).filter((e) => e !== false);
 }
 
 
