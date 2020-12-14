0a5fa76be5a302e85f1f2432487bc8e16068845f
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-12-14 23:13:34 +0300

    feat: add solution to the array tasl

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -202,8 +202,8 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.join('\n');
 }
 
 /**
@@ -236,8 +236,12 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let count = 0;
+  return arr.map((e) => {
+    count += e;
+    return count;
+  });
 }
 
 /**
